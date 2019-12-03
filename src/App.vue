5070ffac73d2ef786879640251ea2c45c7316fc4
hy530845826 <530845826@qq.com>
2019-12-03 15:16:52 +0800

    Fix #1: basic configuration

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <main-interface />
+  </div>
+</template>
+
+<script>
+import MainInterface from './components/MainInterface.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MainInterface
+  }
+}
+</script>
+
+<style>
+  @import "./assets/scss/main.scss";
+</style>
---
+++ src/components/MainInterface.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="content" class="GameBox">
+    <div id="map" class="map-1"></div>
+    <div id="Setting">
+      <div id="Blank3">
+        <div id="BlankHP" class="BlankBox">
+          <p>精</p>
+        </div>
+        <div id="BlankMP" class="BlankBox">
+          <p>气</p>
+        </div>
+        <div id="BlankEXP" class="BlankBox">
+          <p>神</p>
+        </div>
+      </div>
+      <div id="Level">LV.1</div>
+      <div id="Menu">
+        <ul id="Menuul">
+          <li id="MenuJ">委托(J)</li>
+          <li id="MenuI">角色(I)</li>
+          <li id="MenuB">包裹(B)</li>
+          <li id="MenuK">技能(K)</li>
+          <li id="MenuM">地图(M)</li>
+          <li id="MenuP">设置(P)</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "test"
+};
+</script>
