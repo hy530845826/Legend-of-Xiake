1692fc564261d4c9a6edbe8d4052049aa34cc096
hy530845826 <530845826@qq.com>
2020-04-07 12:17:49 +0800

    Fix #26-2: update Home css

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="Home">
-    <div id="logoimg"></div>
-    <div @click="$goRoute('/Game')">开始游戏</div>
-    <div>读取游戏</div>
-    <div>游戏说明</div>
+    <div id="HomeMenu">
+      <div id="logoimg"></div>
+      <ul>
+        <li @click="$goRoute('/Game')">开始游戏</li>
+        <li>读取游戏</li>
+        <li>游戏说明</li>
+      </ul>
+    </div>
     <audio src="./static/sound/home.mp3" class="bgmusic" autoplay></audio>
   </div>
 </template>
